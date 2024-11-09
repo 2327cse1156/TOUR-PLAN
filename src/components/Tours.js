6c54138bac2a957186335d9984e24a10a6709cd1
2327cse1156 <166204708+2327cse1156@users.noreply.github.com>
2024-11-09 23:47:46 +0530

    Adding my first project
    I have made this project using html, css , javascript and react

---
+++ src/components/Tours.js
@@ -0,0 +1,20 @@
+import Card from "./Card";
+function Tours({tours,removeTour})
+{
+    return (
+      <div className="container">
+          <div><h2 className="title">Plan to Enjoy</h2></div>
+        <div className="cards">
+            {
+                tours.map( (tour) =>{
+                    return <Card {...tour} key={tour.id} removeTour={removeTour}></Card>
+                })
+            }
+        </div>
+      </div>
+
+    );
+    
+}
+
+export default Tours;
